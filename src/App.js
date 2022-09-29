343669fc4a368bc18f1f24d534a03045880bc3b2
mostafiz <rafi11zxy@gmail.com>
2022-09-29 23:22:14 +0600

    question update and color change

--- src/App.js
+++ src/App.js
@@ -12,19 +12,13 @@ function App () {
       <h1 className='welcome-text'>Welcome to the page</h1>
       <div className='answer-container'>
         <h2>How does react work?</h2>
-        <p>
-          ReactJS divides the UI into isolated reusable pieces of code known as
-          components. React components work similarly to JavaScript functions as
-          they accept arbitrary inputs called properties or props. It's possible
-          to have as many components as necessary without cluttering your code.
+        <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
         </p>
 
         <h2>Difference between props and state?</h2>
         <p>
-          Props are used to pass data from one component to another. The state
-          is a local data storage that is local to the component only and cannot
-          be passed to other components. The this. setState property is used to
-          update the state values in the component.
+        PROPS: The Data is passed from one component to another. It is Immutable (cannot be modified). Props can be used with state and functional components.<br></br>
+        STATE: The Data is passed within the component only. It is Mutable ( can be modified). State can be used only with the state components/class component.
         </p>
 
         <h2>What does useEffect do without data load?</h2>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,6 @@ const Header = () => {
   return (
     <div>
       <div className='header'>
-        <img
-          className='header-logo'
-          src='https://i.postimg.cc/MGDkYdc0/5eb3cb4bc8c459000443515c.png'
-          alt=''
-        />
         <h3 className='gym-name'>Power GYM</h3>
       </div>
       <h4 className='body-text'>Select your exercise for today & find total time</h4>
