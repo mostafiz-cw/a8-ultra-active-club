5b3139c08537ed367c0fca1f942d584b8e767ded
mostafiz <rafi11zxy@gmail.com>
2022-09-29 10:49:51 +0600

    card real data updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './component/card';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card></Card>
     </div>
   );
 }
---
+++ src/component/card.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const card = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, consequuntur.</p>
+        </div>
+    );
+};
+
+export default card;
